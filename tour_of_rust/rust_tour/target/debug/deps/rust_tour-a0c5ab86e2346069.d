c45b4c77c6b9d1a3cb02380c4529eaa323917844
statueofdavid <dmpersonalemail@gmail.com>
2022-07-22 23:03:59 -0400

    added tests for gcd

---
+++ tour_of_rust/rust_tour/target/debug/deps/rust_tour-a0c5ab86e2346069.d
@@ -0,0 +1,5 @@
+/home/divad/my-rust-journey/tour_of_rust/rust_tour/target/debug/deps/rust_tour-a0c5ab86e2346069: src/main.rs
+
+/home/divad/my-rust-journey/tour_of_rust/rust_tour/target/debug/deps/rust_tour-a0c5ab86e2346069.d: src/main.rs
+
+src/main.rs:
